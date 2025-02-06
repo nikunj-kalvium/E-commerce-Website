44528f7f26da165c214cace4fd153fff15c6649d
Nikunj <nikunjkohli10@gmail.com>
2025-02-06 16:07:14 +0530

    sign-up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {LoginPage} from "./Routess.js"// Import fixed
+import {LoginPage,SignUpPage} from "./Routess.js"// Import fixed
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/SignUpPage" element={<SignUpPage />} />
         
       </Routes>
     </BrowserRouter>
--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -1,9 +1,10 @@
 import LoginPage from "../src/pages/login.jsx"
+import SignUpPage from "../src/pages/SignUpPage.jsx"
 
 
 export
 {
     LoginPage,
-   
+    SignUpPage,
 }
 
